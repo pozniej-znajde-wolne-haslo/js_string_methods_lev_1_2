51c37f42c7946dfe90a76b9928f3777b5a828733
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-24 21:44:48 +0100

    js_string methods_indexOf

---
+++ script.js
@@ -0,0 +1,16 @@
+const txt = "How inappropriate to call this planet Earth, when clearly it is Ocean.";
+
+console.log(txt.indexOf("h")); // 'h' in 'this'
+console.log(txt.indexOf("Earth")); // wo fängt es an (Position '38') - starts counting with '0'
+console.log(txt.indexOf("Moon")); // not in the string, so '-1'
+
+
+
+/* Verwende den Befehl indexOf(), um die Position eines Zeichens in einer Zeichenkette als Ergebnis in der Konsole zu erhalten.
+
+const txt = ”How inappropriate to call this planet Earth, when clearly it is Ocean.”
+
+Suche die Position des Zeichens “h” in der Variablen txt.
+Suche die Position des Zeichens “Earth” in der Variablen txt.
+Suche die Position des Zeichens “Moon” in der Variablen txt.
+ */
